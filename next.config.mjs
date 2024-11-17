e499fe5ccb81a4459546b8f038245a1f6843b698
remjx <35121685+remjx@users.noreply.github.com>
2024-11-17 18:05:01 -0500

    redirect all to app

--- next.config.mjs
+++ next.config.mjs
@@ -3,41 +3,10 @@ const nextConfig = {
     async redirects() {
         return [
           {
-            source: '/api/:path*',
-            destination: 'https://app.bamk.fi/api/:path*',
+            source: '/:path*',
+            destination: 'https://app.bamk.fi/:path*',
             permanent: false,
           },
-          {
-            source: '/calculator',
-            destination: 'https://app.bamk.fi/calculator',
-            permanent: false,
-          },
-          {
-            source: '/leaderboard',
-            destination: 'https://app.bamk.fi/leaderboard',
-            permanent: false,
-          },
-          {
-            source: '/swap',
-            destination: 'https://app.bamk.fi/swap',
-            permanent: false,
-          },
-          {
-            source: '/mint',
-            destination: 'https://app.bamk.fi/swap/mint',
-            permanent: false,
-          },
-          {
-            source: '/redeem',
-            destination: 'https://app.bamk.fi/swap/redeem',
-            permanent: false,
-          },
-          {
-            source: '/swap/:path',
-            destination: 'https://app.bamk.fi/swap/:path*',
-            permanent: false,
-          },
-          
         ];
       },
 };
